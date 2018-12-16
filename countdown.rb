4c2a19ae494bfca64bf8643bb6e0817de82ea0e3
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-16 23:15:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(num)
   while num>=0
-    puts "#{num} SECOND(S)!"
+    "#{num} SECOND(S)!"
     num-=1
   end
   "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep(num)
     while num>=0
-    puts "#{num} SECOND(S)!"
+    "#{num} SECOND(S)!"
     num-=1
     sleep 1
   end
