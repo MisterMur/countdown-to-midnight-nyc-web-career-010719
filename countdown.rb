c71a696e3a17f3148f541b16916ef8c26cb6c193
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-16 23:09:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,10 @@ def countdown(num)
   while num>=0
     puts "#{num} SECOND(S)!"
     num-=1
+    countdown_with_sleep(1)
   end
 end
 
-def countdown_with_sleep
-  sleep 
+def countdown_with_sleep(s)
+  sleep s
+end
